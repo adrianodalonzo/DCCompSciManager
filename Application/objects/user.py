052e3d145dc0f5f46c448244eae5a932962b895e
Vincent Keedwell <vincent.keedwell@dawsoncollege.qc.ca>
2023-03-29 15:35:41 -0400

    Added base User and UserForm classes

---
+++ Application/objects/user.py
@@ -0,0 +1,37 @@
+class User:
+    def __init__(self, email, name, password, avatarlink):
+        if email is not isinstance(str):
+            raise TypeError("email must be a string")
+
+        if name.isalnum() == False:
+            raise TypeError("name must only contain alphanumeric characters")
+
+        if password is not isinstance(str):
+            raise TypeError("password must be a string")
+
+        if avatarlink is not isinstance(str):
+            raise TypeError("avatarlink must be a string")
+        
+        self.email = email
+        self.name = name
+        self.password = password
+        self.avatarlink = avatarlink
+
+        self.id = None
+        self.group = None
+    
+    def __repr__(self):
+        return f'User: ({self.email}, {self.name}, {self.password}, {self.avatarlink}, {self.id}, {self.group})'
+    
+    def __str__(self):
+        return f'Email: {self.email}, Name: {self.name}, Password: {self.password}, AvatarLink: {self.avatarlink}, ID: {self.id}, Group: {self.group}'
+
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordField
+from wtforms.validators import DataRequired, Length
+class UserForm(FlaskForm):
+    email = EmailField(DataRequired(), Length(min=3, max=100))
+    name = StringField(DataRequired(), Length(min=1, max=100))
+    password = PasswordField(DataRequired(), Length(min=1))
+    avatarlink = StringField()

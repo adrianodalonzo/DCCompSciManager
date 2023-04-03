74090ee6dc3cc9290883640b6ecdb2cb73d93825
adridalo <adriano.dalonzo@dawsoncollege.qc.ca>
2023-04-03 10:30:04 -0400

    Added login config in init.py

--- Application/__init__.py
+++ Application/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import secrets
 from .dbmanager import get_db
+from flask_login import LoginManager
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -16,6 +17,14 @@ def create_app(test_config=None):
     
     from .dbmanager import init_db_command
     app.cli.add_command(init_db_command)
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return get_db().get_user_id(int(user_id))
     
     return app
     

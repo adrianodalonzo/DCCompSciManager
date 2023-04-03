b222db1e5eb26384308785e55aff239476416e89
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2023-04-03 13:32:07 -0400

    commented out code for validating length of cursor

--- Application/db.py
+++ Application/db.py
@@ -50,11 +50,11 @@ class Database:
         if not isinstance(email, str):
             raise TypeError('Email MUST be a string!')
         
-        with self.__get_cursor() as cursor:
-            results = cursor.execute("""select email, password, user_id, username, user_group, avatar_path from courses_users
-                                     where email = :email""", email=email)
-            if results.rowcount == 0:
-                raise oracledb.DataError
+        # with self.__get_cursor() as cursor:
+        #     results = cursor.execute("""select email, password, user_id, username, user_group, avatar_path from courses_users
+        #                              where email = :email""", email=email)
+        #     if results.rowcount == 0:
+        #         raise oracledb.DataError
 
         with self.__get_cursor() as cursor:
             results = cursor.execute("""select email, password, user_id, username, user_group, avatar_path from courses_users
@@ -69,11 +69,11 @@ class Database:
         if not isinstance(id, int):
             raise TypeError('ID MUST be a number!!')
         
-        with self.__get_cursor() as cursor:
-            results = cursor.execute("""select email, password, user_id, username, user_group, avatar_path from courses_users
-                                     where user_id = :id""", id=id)
-            if results.rowcount == 0:
-                raise oracledb.DataError
+        # with self.__get_cursor() as cursor:
+        #     results = cursor.execute("""select email, password, user_id, username, user_group, avatar_path from courses_users
+        #                              where user_id = :id""", id=id)
+        #     if results.rowcount == 0:
+        #         raise oracledb.DataError
         
         with self.__get_cursor() as cursor:
             results = cursor.execute("""select email, password, user_id, username, user_group, avatar_path from courses_users

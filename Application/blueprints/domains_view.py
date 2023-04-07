8a3c52cd5de079d2ae453057b65fb08a3e59ee92
unknown <shu.liu1@dawsoncollege.qc.ca>
2023-04-07 15:45:04 -0400

    added domain display

--- Application/blueprints/domains_view.py
+++ Application/blueprints/domains_view.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
+from ..dbmanager import get_db
 
 bp = Blueprint("domains", __name__, url_prefix="/domains/")
 
 @bp.route("/", methods=['GET', 'POST'])
 def show_domains():
-    return render_template('domains.html')
+    domains = get_db().get_all_domains()
+    if domains :
+        return render_template('domains.html', domains=domains)
+    
+    flash("No domains")
+    return render_template('index.html')
+
+@bp.route("/<int:domain_id>", methods=['GET'])
+def show_domain(domain_id):
+    domain = get_db().get_domain(domain_id)
+    if domain:
+        return render_template('domains.html', domain=domain)
+
+    return render_template('index.html')

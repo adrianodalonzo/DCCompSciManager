6ec17ad6c561162d902cb810d27fee5ca08551dc
unknown <shu.liu1@dawsoncollege.qc.ca>
2023-04-06 23:03:34 -0400

    created show courses display linked with database

--- Application/blueprints/courses_view.py
+++ Application/blueprints/courses_view.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
+from ..dbmanager import get_db
 
 bp = Blueprint("courses", __name__, url_prefix="/courses/")
 
 @bp.route("/", methods=['GET', 'POST'])
 def show_courses():
+    courses = get_db().get_all_courses()
+    if courses:
+        return render_template('courses.html', courses=courses)
+    flash("No courses")
     return render_template('courses.html')

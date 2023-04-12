75513185ee918e7c575ee8d28ac66b5812e15e54
adridalo <adriano.dalonzo@dawsoncollege.qc.ca>
2023-04-12 15:12:52 -0400

    updated user object to not take in an avatar link

--- Application/objects/user.py
+++ Application/objects/user.py
@@ -1,7 +1,7 @@
 from flask_login import UserMixin
 
 class User(UserMixin):
-    def __init__(self, email, name, password, avatarlink):
+    def __init__(self, email, name, password):
         if not isinstance(email, str):
             raise TypeError("email must be a string")
 
@@ -10,23 +10,19 @@ class User(UserMixin):
 
         if not isinstance(password, str):
             raise TypeError("password must be a string")
-
-        if not isinstance(avatarlink, str):
-            raise TypeError("avatarlink must be a string")
         
         self.email = email
         self.name = name
         self.password = password
-        self.avatarlink = avatarlink
 
         self.id = None
         self.group = None
     
     def __repr__(self):
-        return f'User: ({self.email}, {self.name}, {self.password}, {self.avatarlink}, {self.id}, {self.group})'
+        return f'User: ({self.email}, {self.name}, {self.password}, {self.id}, {self.group})'
     
     def __str__(self):
-        return f'Email: {self.email}, Name: {self.name}, Password: {self.password}, AvatarLink: {self.avatarlink}, ID: {self.id}, Group: {self.group}'
+        return f'Email: {self.email}, Name: {self.name}, Password: {self.password}, ID: {self.id}, Group: {self.group}'
 
 
 from flask_wtf import FlaskForm

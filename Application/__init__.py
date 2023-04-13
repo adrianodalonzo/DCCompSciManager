1e078a80d7611e02535ad2822eb4a491c87df124
adridalo <adriano.dalonzo@dawsoncollege.qc.ca>
2023-04-13 10:47:29 -0400

    registered blueprint

--- Application/__init__.py
+++ Application/__init__.py
@@ -39,6 +39,8 @@ def make_blueprints(app):
     app.register_blueprint(home_bp)
     from .blueprints.auth_views import bp as auth_bp
     app.register_blueprint(auth_bp)
+    from .blueprints.profile_views import bp as profile_bp
+    app.register_blueprint(profile_bp)
     
 def cleanup(value):
     get_db().close()

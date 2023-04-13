6d6c9072a36bb6582d24c1968427b70610a20c04
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2023-04-13 13:37:37 -0400

    added code to reset password

--- Application/blueprints/profile_views.py
+++ Application/blueprints/profile_views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required
+from ..objects.user import ResetPasswordForm
 from ..dbmanager import get_db
+from werkzeug.security import check_password_hash, generate_password_hash
 
 bp = Blueprint('profile', __name__, url_prefix='/profile/')
 
@@ -11,7 +13,28 @@ def get_profile(email):
         raise Exception('Email MUST be a string!')
     return render_template('profile.html')
 
-@bp.route('/reset-password/')
+@bp.route('/reset-password/', methods=['GET', 'POST'])
 @login_required
 def reset_password():
-    return render_template('reset_password.html')
+    form = ResetPasswordForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            old_password = form.old_password.data
+            if check_password_hash(current_user.password, old_password):
+                new_password = form.new_password.data
+                retyped_new_password = form.retype_new_password.data
+                if new_password == retyped_new_password:
+                    hashed_password = generate_password_hash(new_password)
+                    get_db().update_user_password(current_user.email, hashed_password)
+                    flash('Password Successfully Resetted!', category='valid')
+                    return render_template('profile.html')
+                else:
+                    flash("The New Password and Retyped New Password Must Both Match!", category='invalid')
+                    return redirect(url_for('profile.reset_password'))
+            else:
+                flash("The Password You Entered Doesn't Match Your Old Password!", category='invalid')
+                return redirect(url_for('profile.reset_password'))
+        else:
+            flash('Form Is Invalid!', category='invalid')
+    elif request.method == 'GET':
+        return render_template('reset_password.html', form=form)

4ba6b1ad9da21a95331052718f3fa9d8a3b2af41
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2023-04-13 13:20:56 -0400

    created form for resetting password

--- Application/objects/user.py
+++ Application/objects/user.py
@@ -42,3 +42,7 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me?')
     
 #TODO: Add form here for resetting a password
+class ResetPasswordForm(FlaskForm):
+    old_password = PasswordField('Old Password', validators=[DataRequired(), Length(min=8)])
+    new_password = PasswordField('New Password', validators=[DataRequired(), Length(min=8)])
+    retype_new_password = PasswordField('Retype New Password', validators=[DataRequired(), Length(min=8)])

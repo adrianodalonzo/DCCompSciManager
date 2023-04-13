82b73ed90791a96faaa40f6ea770334b07c2649c
Vincent Keedwell <vincent.keedwell@dawsoncollege.qc.ca>
2023-04-13 10:17:01 -0400

    Added add course form

--- Application/blueprints/courses_view.py
+++ Application/blueprints/courses_view.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, flash
+
+from Application.objects.course import Course, CourseForm
 from ..dbmanager import get_db
 
 bp = Blueprint("courses", __name__, url_prefix="/courses/")
@@ -11,7 +13,7 @@ def show_courses():
     flash("No courses")
     return render_template('index.html')
 
-@bp.route("/domain/<int:domain_id>", methods=['GET', 'POST'])
+@bp.route("/domain/<int:domain_id>/", methods=['GET', 'POST'])
 def show_courses_by_domain(domain_id):
     courses = get_db().get_courses_by_domain(domain_id)
     if courses:
@@ -20,10 +22,33 @@ def show_courses_by_domain(domain_id):
     flash("No courses by this domain")
     return render_template('index.html')
 
-@bp.route("/term/<int:term_id>", methods=['GET', 'POST'])
+@bp.route("/term/<int:term_id>/", methods=['GET', 'POST'])
 def show_courses_by_term(term_id):
     courses = get_db().get_courses_by_term(term_id)
     if courses:
         return render_template('courses.html', courses=courses, term=term_id)
     flash("No courses by this term")
     return render_template('index.html')
+
+@bp.route("/add/", methods=['GET', 'POST'])
+def add_course():
+    form = CourseForm()
+
+    if request.method == 'GET':
+        return render_template('modify_course.html', form=form)
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            matchingCourse = False
+
+            for course in get_db().get_all_courses():
+                if course.id == form.id.data:
+                    matchingCourse = True
+                    flash("A Course with the same number already exists!")
+            
+            if matchingCourse:
+                course = Course(form.id.data, form.title.data, form.theory_hours.data, 
+                                form.lab_hours.data, form.work_hours.data, 
+                                form.description.data, form.domain_id.data, form.term_id.data)
+                # get_db().add_course(course)
+                # ^ not implemented yet

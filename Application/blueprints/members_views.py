d3f6dc610d3acecd1e9537582712e440ccbc419a
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2023-04-15 16:46:56 -0400

    passed signup form to template

--- Application/blueprints/members_views.py
+++ Application/blueprints/members_views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 
+from ..objects.user import SignUpForm
+
 from ..dbmanager import get_db
 
 bp = Blueprint('members', __name__, url_prefix='/members/')
@@ -9,4 +11,5 @@ bp = Blueprint('members', __name__, url_prefix='/members/')
 @login_required
 def list_members():
     members = get_db().get_members()
-    return render_template('members.html', members=members)
+    add_form = SignUpForm()
+    return render_template('members.html', members=members, add_form=add_form)

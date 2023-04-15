18c70b22d2651c8737dc37d205d69afe7dd7b879
Vincent Keedwell <vincent.keedwell@dawsoncollege.qc.ca>
2023-04-14 22:42:54 -0400

    Added routes for individual competencies

--- Application/blueprints/competencies_view.py
+++ Application/blueprints/competencies_view.py
@@ -1,22 +1,23 @@
 from flask import Blueprint, redirect, render_template, request, flash, url_for
 
-from Application.objects.competency import Competency
+from Application.objects.competency import Competency, CompetencyForm
 from ..dbmanager import get_db
 
 bp = Blueprint("competencies", __name__, url_prefix="/competencies/")
 
-@bp.route("/<string:course_id>/", methods=['GET', 'POST'])
-def show_course_competencies(course_id):
-    if request.method == 'GET':
-        competencies = get_db().get_course_competencies(course_id)
-        if competencies:
-            elements_array = []
-            for competency in competencies:
-                elements_array.append(get_db().get_competency_elements(competency.id))
-            return render_template('competencies.html', competencies=competencies, elements_array=elements_array)
+# Should prob be in courses_view
+# @bp.route("/<string:course_id>/", methods=['GET', 'POST'])
+# def show_course_competencies(course_id):
+#     if request.method == 'GET':
+#         competencies = get_db().get_course_competencies(course_id)
+#         if competencies:
+#             elements_array = []
+#             for competency in competencies:
+#                 elements_array.append(get_db().get_competency_elements(competency.id))
+#             return render_template('competencies.html', competencies=competencies, elements_array=elements_array)
         
-        flash("The competencies does not exist.")
-    return render_template('index.html')
+#         flash("The competencies does not exist.")
+#     return render_template('index.html')
 
 @bp.route("/")
 def show_all_competencies():
@@ -30,9 +31,34 @@ def show_all_competencies():
 @bp.route("/<string:comp_id>/")
 def show_competency_elements(comp_id):
     competency = get_db().get_competency(comp_id)
+    competencies = []
+    competencies.append(competency)
     elements = get_db().get_competency_elements(comp_id)
+    elements_array = []
+    elements_array.append(elements)
 
     if competency:
-        return render_template('competencies.html', competencies=competency, elements_array=elements)
+        return render_template('competencies.html', competencies=competencies, elements_array=elements_array)
     flash('No Competency Found')
     return redirect(url_for('show_all_competencies'))
+
+@bp.route("/add/", methods=['GET', 'POST'])
+def add_competency():
+    form = CompetencyForm()
+
+    if request.method == 'GET':
+        return render_template('modify_competency.html')
+    
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            matchingCompetency = False
+
+            for competency in get_db().get_all_competencies():
+                if competency.id == form.id.data:
+                    matchingCompetency = True
+                    flash("A Competency with the same id already exists!")
+            
+            if not matchingCompetency:
+                comp = Competency(form.id.data, form.name.data,
+                                  form.achievement.data, form.type.data)
+                get_db().add_competency(comp)

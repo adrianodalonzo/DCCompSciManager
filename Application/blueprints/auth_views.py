446aede16795ff20818f582dd822ed4442346d5c
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2023-04-21 23:46:56 -0400

    commented out code which was giving some issues when getting avatar for blocked user

--- Application/blueprints/auth_views.py
+++ Application/blueprints/auth_views.py
@@ -102,8 +102,8 @@ def logout():
 @bp.route('/avatar/<email>/avatar.png/')
 @login_required
 def get_avatar(email):
-    if current_user.blocked:
-        flash("You Have Been Blocked by an Admin, so Viewing this Page is Not Allowed!", category='invalid')
-        return redirect(url_for('profile.profile'))
+    # if current_user.blocked:
+    #     flash("You Have Been Blocked by an Admin, so Viewing this Page is Not Allowed!", category='invalid')
+    #     return redirect(url_for('profile.profile'))
     dir = os.path.join(current_app.config['IMAGE_PATH'], email)
     return send_from_directory(dir, 'avatar.png')

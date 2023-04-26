b6a3163d389ea7e5b6623de202567c9ce3e627af
Vincent Keedwell <vincent.keedwell@dawsoncollege.qc.ca>
2023-04-26 15:30:29 -0400

    Add routes for add/edit/del domains

--- Application/blueprints/domains_view.py
+++ Application/blueprints/domains_view.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, redirect, render_template, request, flash, url_for
 from flask_login import current_user, login_required
 
-from Application.objects.domain import DomainForm
+from Application.objects.domain import Domain, DomainForm
 from ..dbmanager import get_db
 
 bp = Blueprint("domains", __name__, url_prefix="/domains/")
@@ -26,7 +26,7 @@ def show_domain(domain_id):
     flash("No domain with this id", category='invalid')
     return render_template('index.html')
 
-@bp.route('/domains/add/', methods=['GET', 'POST'])
+@bp.route('/add/', methods=['GET', 'POST'])
 @login_required
 def add_domain():
     form = DomainForm()
@@ -36,3 +36,52 @@ def add_domain():
     
     elif request.method == 'POST':
         if form.validate_on_submit():
+            matchingDomain = False
+
+            for domain in get_db().get_all_domains():
+                if domain.name == form.name.data:
+                    matchingDomain = True
+                    flash("A Domain with the same name already exists")
+
+            if not matchingDomain:
+                dom = Domain(form.name.data, form.description.data)
+                get_db().add_domain(dom)
+                flash("Added Domain: " + dom.name, category='valid')
+
+    return redirect(url_for('domains.show_domains'))
+
+@bp.route("/edit/<int:dom_id>/")
+@login_required
+def edit_domain(dom_id):
+    dom = get_db().get_domain(dom_id)
+    form = DomainForm(obj=dom)
+
+    if request.method == 'GET':
+        return render_template('modify_domain.html', form=form, domain=dom)
+    
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            dom_name = form.name.data
+            dom_desc = form.description.data
+
+            if form.name.data is None:
+                dom_name = dom.name
+            if form.description.data is None:
+                dom_desc = dom.description
+            
+            domain = Domain(dom_name, dom_desc)
+            domain.id = dom.id
+            get_db().modify_domain(domain)
+            flash("Edited Domain: " + domain.name, category='valid')
+        
+        else:
+            flash("Domain form is not valid!", category='invalid')
+    
+    return redirect(url_for('domains.show_domains'))
+
+@bp.route('/delete/<int:dom_id>')
+@login_required
+def delete_domain(dom_id):
+    get_db().delete_domain(dom_id)
+    flash("Domain " + dom_id + " has been deleted along with its courses")
+    return redirect(url_for('domains.show_domains'))

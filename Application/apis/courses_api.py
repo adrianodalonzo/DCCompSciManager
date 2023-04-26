0a30244064a0e3d83d19b1c75772b991272fe45d
li yaya <shu.liu1@dawsoncollege.qc.ca>
2023-04-26 14:13:47 -0400

    working on course api

--- Application/apis/courses_api.py
+++ Application/apis/courses_api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, flash, make_response, url_for
 from ..dbmanager import get_db
 from Application.objects.course import Course
+import oracledb
 bp = Blueprint('courses_api', __name__, url_prefix='/api/courses')
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -32,13 +33,43 @@ def course_api(course_id):
             courses_json = request.json
             if courses_json:
                 course = Course.from_json(courses_json)
+                existing_element = get_db().get_course(course.id)
+                if existing_element:
+                    get_db().modify_course(course)
+                    infoset = {'id': "Success", 'description': 'Successfully updated course.'}
+                    return make_response(jsonify(infoset), 200)
                 get_db().add_course(course)
-                response = make_response({}, 201)
-                return response
+                infoset = {'id': "Success", 'description': 'Successfully added course.'}
+                return make_response(jsonify(infoset), 201)
+            
         elif request.method == 'GET':
-            course = get_db().get_course(course_id)
-            url = url_for('courses_api.course_api', course_id=course_id)
-            domain_url = url_for('domains_api.domain_api', domain_id=course.domain_id)
-            return jsonify(course.to_json(url, domain_url))
+            try:
+                course = get_db().get_course(course_id)
+                url = url_for('courses_api.course_api', course_id=course_id)
+                domain_url = url_for('domains_api.domain_api', domain_id=course.domain_id)
+                return jsonify(course.to_json(url, domain_url))
+            except Exception:
+                error_infoset = {'id': 'Bad Request',
+                        'description': 'Element not found, please insert a valid element id.'}
+                return make_response(jsonify(error_infoset), 400)
+        
+        elif request.method == 'DELETE':
+            try:
+                course = get_db().get_course(course_id)
+                get_db().delete_course(course_id)
+                infoset = {'id': "Success", 'description': 'Successfully deleted course'}
+                return make_response(jsonify(infoset), 204)
+            except Exception:
+                error_infoset = {'id': 'Bad Request',
+                        'description': 'Course not found, please insert a valid course id.'}
+                return make_response(jsonify(error_infoset), 400)
+    
+    except oracledb.Error:
+        error_infoset = {'id': 'Internal Service Error',
+                        'description': 'There is problems in the database. Please try again later.'}
+        return make_response(jsonify(error_infoset), 500)
+    
     except Exception:
-        return ""
+        error_infoset = {'id': 'Not Found',
+                        'description': 'Url not found on this server.'}
+        return make_response(jsonify(error_infoset), 404)

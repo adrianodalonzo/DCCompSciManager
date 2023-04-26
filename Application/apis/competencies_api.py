e0992455a4196f5ca3ca31d4822b7179c2d12217
li yaya <shu.liu1@dawsoncollege.qc.ca>
2023-04-26 13:57:15 -0400

    added and improved post, put, get and delete for competency api

--- Application/apis/competencies_api.py
+++ Application/apis/competencies_api.py
@@ -13,17 +13,27 @@ def competencies_api():
             if competencies_json:
                 competency = Competency.from_json(competencies_json)
                 get_db().add_competency(competency)
+                infoset = {'id': "Success", 'description': 'Successfully added competency.'}
+                return make_response(jsonify(infoset), 201)
+            
         elif request.method == 'GET':
             if request.args:
                 id = request.args.get("id")
-                competencies = get_db().get_all_competencies()
-                competency = [competency for competency in competencies if competency.id == id]
-                return jsonify(competency[0].__dict__)
+                competency = get_db().get_competency(id)
+                return jsonify(competency.to_json())
         competencies = get_db().get_all_competencies()
         json = [competency.__dict__ for competency in competencies]
         return jsonify(json)
+
+    except oracledb.Error:
+        error_infoset = {'id': 'Internal Service Error',
+                        'description': 'There is problems in the database. Please try again later.'}
+        return make_response(jsonify(error_infoset), 500)
+    
     except Exception:
-        return ""
+        error_infoset = {'id': 'Not Found',
+                        'description': 'Url not found on this server.'}
+        return make_response(jsonify(error_infoset), 404)
     
 @bp.route('/<competency_id>', methods=['GET', 'PUT', 'DELETE'])
 def competency_api(competency_id):
@@ -32,50 +42,74 @@ def competency_api(competency_id):
             competencies_json = request.json
             if competencies_json:
                 competency = Competency.from_json(competencies_json)
+                existing_competency = get_db().get_competency(competency.id)
+                if existing_competency:
+                    get_db().modify_competency(competency)
+                    infoset = {'id': "Success", 'description': 'Successfully updated competency.'}
+                    return make_response(jsonify(infoset), 200)
                 get_db().add_competency(competency)
-        elif request.method == 'GET':
-            competency = get_db().get_competency(competency_id)
-            return jsonify(competency.__dict__)
-        
-        elif request.method == 'DELETE':
-            pass
-    except Exception:
-        return ""
-    
-@bp.route('/<competency_id>/elements', methods=['GET', 'POST'])
-def competency_elements_api(competency_id):
-    try:
-        if request.method == 'PUT':
-            elements_json = request.json
-            if elements_json:
-                element = Element.from_json(elements_json)
-                get_db().add_competency_element(element)
-                infoset = {'id': "Success", 'description': 'Successfully updated element'}
-                return make_response(infoset, 201)
-            
+                infoset = {'id': "Success", 'description': 'Successfully added competency'}
+                return make_response(jsonify(infoset), 201)
+
         elif request.method == 'GET':
             try:
-                if request.args:
-                    id = request.args.get("id")
-                    element = get_db().get_element(id)
-                    return jsonify(element.to_json())
-                elements = get_db().get_competency_elements(competency_id)
-                json = [element.__dict__ for element in elements]
-                return jsonify(json)
+                competency = get_db().get_competency(competency_id)
+                return jsonify(competency.to_json())
             except Exception:
-                    error_infoset = {'id': 'Not Found',
-                            'description': 'Url not found on this server.'}
-                    return make_response(jsonify(error_infoset), 404)
+                error_infoset = {'id': 'Bad Request',
+                        'description': 'Element not found, please insert a valid element id.'}
+                return make_response(jsonify(error_infoset), 400)
+        
+        elif request.method == 'DELETE':
+            try:
+                competency = get_db().get_competency(competency_id)
+                get_db().delete_competency(competency)
+                infoset = {'id': "Success", 'description': 'Successfully deleted competency.'}
+                return make_response(jsonify(infoset), 204)
+            except Exception:
+                error_infoset = {'id': 'Bad Request',
+                        'description': 'Competency not found, please insert a valid competency id.'}
+                return make_response(jsonify(error_infoset), 400)
     
     except oracledb.Error:
         error_infoset = {'id': 'Internal Service Error',
                         'description': 'There is problems in the database. Please try again later.'}
-        return make_response(error_infoset, 500)
+        return make_response(jsonify(error_infoset), 500)
     
     except Exception:
+        error_infoset = {'id': 'Not Found',
+                        'description': 'Url not found on this server.'}
+        return make_response(jsonify(error_infoset), 404)
+    
+@bp.route('/<competency_id>/elements', methods=['GET', 'POST'])
+def competency_elements_api(competency_id):
+    try:
+        if request.method == 'POST':
+            elements_json = request.json
+            if elements_json:
+                element = Element.from_json(elements_json)
+                get_db().add_competency_element(element)
+                infoset = {'id': "Success", 'description': 'Successfully added element'}
+                return make_response(jsonify(infoset), 201)
+            
+        elif request.method == 'GET':
+            if request.args:
+                id = request.args.get("id")
+                element = get_db().get_element(id)
+                return jsonify(element.to_json())
+            elements = get_db().get_competency_elements(competency_id)
+            json = [element.__dict__ for element in elements]
+            return jsonify(json)
+    
+    except oracledb.Error:
         error_infoset = {'id': 'Internal Service Error',
                         'description': 'There is problems in the database. Please try again later.'}
         return make_response(jsonify(error_infoset), 500)
+    
+    except Exception:
+        error_infoset = {'id': 'Not Found',
+                        'description': 'Url not found on this server.'}
+        return make_response(jsonify(error_infoset), 404)
     
 @bp.route('/<competency_id>/elements/<int:element_id>', methods=['GET', 'PUT', 'DELETE'])
 def competency_element_api(competency_id, element_id):
@@ -116,7 +150,7 @@ def competency_element_api(competency_id, element_id):
     except oracledb.Error:
         error_infoset = {'id': 'Internal Service Error',
                         'description': 'There is problems in the database. Please try again later.'}
-        return make_response(error_infoset, 500)
+        return make_response(jsonify(error_infoset), 500)
     
     except Exception:
         error_infoset = {'id': 'Not Found',

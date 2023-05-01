f1e5d7e45601debbe007b5be229d91e074be4aa9
Vincent Keedwell <vincent.keedwell@dawsoncollege.qc.ca>
2023-05-01 10:02:39 -0400

    Removed block of comments

--- Application/blueprints/competencies_view.py
+++ Application/blueprints/competencies_view.py
@@ -8,20 +8,6 @@ from ..dbmanager import get_db
 
 bp = Blueprint("competencies", __name__, url_prefix="/competencies/")
 
-# Should prob be in courses_view
-# @bp.route("/<string:course_id>/", methods=['GET', 'POST'])
-# def show_course_competencies(course_id):
-#     if request.method == 'GET':
-#         competencies = get_db().get_course_competencies(course_id)
-#         if competencies:
-#             elements_array = []
-#             for competency in competencies:
-#                 elements_array.append(get_db().get_competency_elements(competency.id))
-#             return render_template('competencies.html', competencies=competencies, elements_array=elements_array)
-        
-#         flash("The competencies does not exist.")
-#     return render_template('index.html')
-
 @bp.route("/")
 def show_all_competencies():
     if current_user.is_active:
